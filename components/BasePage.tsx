71210ca7b18a2a8a75209caa0cbc876703f74f49
joonamo <joonamo@users.noreply.github.com>
2022-02-20 16:30:27 +0200

    Analytics

--- components/BasePage.tsx
+++ components/BasePage.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import React from "react"
 
 import { Header, HeaderProps } from "./Header"
@@ -6,6 +7,13 @@ export interface BasePageProps extends HeaderProps {}
 
 export const BasePage: React.FunctionComponent<BasePageProps> = (props) => (
   <>
+    <Head>
+      <script
+        defer
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "fcbdeed83e0f4ad4b978c6c564a61ca1"}'
+      ></script>
+    </Head>
     <Header key="header" {...props} />
     <div key="content" className="jm-container mb-[150px]">
       {props.children}
