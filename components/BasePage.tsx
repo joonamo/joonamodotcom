d3e153436c40392126eaea204bd57383a817864a
joonamo <joonamo@users.noreply.github.com>
2022-06-08 21:35:16 +0300

    Fix warnings

--- components/BasePage.tsx
+++ components/BasePage.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head"
+import Script from "next/script"
 import React from "react"
 
 import { Header, HeaderProps } from "./Header"
@@ -9,13 +9,10 @@ export const BasePage: React.FunctionComponent<
   React.PropsWithChildren<BasePageProps>
 > = (props) => (
   <>
-    <Head>
-      <script
-        defer
-        src="https://static.cloudflareinsights.com/beacon.min.js"
-        data-cf-beacon='{"token": "fcbdeed83e0f4ad4b978c6c564a61ca1"}'
-      ></script>
-    </Head>
+    <Script
+      src="https://static.cloudflareinsights.com/beacon.min.js"
+      data-cf-beacon='{"token": "fcbdeed83e0f4ad4b978c6c564a61ca1"}'
+    ></Script>
     <Header key="header" {...props} />
     <div key="content" className="jm-container mb-[150px]">
       {props.children}
--- components/BreadCrumbs.tsx
+++ components/BreadCrumbs.tsx
@@ -14,10 +14,10 @@ export const BreadCrumbs: React.FunctionComponent<BreadCrumbsProps> = (
 ) => (
   <div className="flex flex-row flex-wrap">
     {props.path.map((item, i) => (
-      <>
+      <React.Fragment key={`${i}-${item.path}`}>
         {i > 0 ? <p className="my-0 mx-2">/</p> : null}
         <Link href={item.path}>{item.name}</Link>
-      </>
+      </React.Fragment>
     ))}
   </div>
 )
